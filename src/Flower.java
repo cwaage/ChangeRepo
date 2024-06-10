bb5fad66070ae88c43da1014cf9cdf092a9db1ec
Chris Waage <40725414+cwaage@users.noreply.github.com>
2024-06-10 14:20:57 -0700

    Added i

--- src/Flower.java
+++ src/Flower.java
@@ -4,11 +4,14 @@ public class Flower {
     int num;
     boolean isRed;
     String mike;
+    String suz;
+    int i;
 
-    public Flower(String color, int num){
+    public Flower(String color, int num, int i){
         this.num = num;
         this.color = color;
         this.isRed = false;
+        this.i = i;
     }
 
     public String toString(){
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("Git Example");
-        Flower f = new Flower("Red", 3);
+        Flower f = new Flower("Red", 3, 55);
 
         System.out.println(f);
 
