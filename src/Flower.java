114c0504a899cac83089307f00a34459d25a6715
Chris Waage <crwaage@gmail.com>
2024-06-10 13:35:42 -0700

    Added toString

--- src/Flower.java
+++ src/Flower.java
@@ -3,9 +3,13 @@ public class Flower {
     String color;
     int num;
 
-    public Flower(String color){
-        this.num = 99;
+    public Flower(String color, int num){
+        this.num = num;
         this.color = color;
+    }
+
+    public String toString(){
+        return "Flower | " + color + "," + num;
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
-package com.waage.ap.csa;
-
 public class Main {
 
     public static void main(String[] args) {
 
         System.out.println("Git Example");
+        Flower f = new Flower("Red", 3);
+
+        System.out.println(f);
 
 
     }
