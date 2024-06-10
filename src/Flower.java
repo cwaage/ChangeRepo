6e5c3bd00339e5856a33782c455dc0bef2c42661
Chris Waage <40725414+cwaage@users.noreply.github.com>
2024-06-10 13:42:20 -0700

    Update Flower.java
    Add boolean

--- src/Flower.java
+++ src/Flower.java
@@ -2,10 +2,13 @@ public class Flower {
 
     String color;
     int num;
+    boolean isPerennial;
 
     public Flower(String color, int num){
         this.num = num;
         this.color = color;
+        isPerennial = false;
+        
     }
 
     public String toString(){
